b01a8e4048e1483b2cba67f3df61e06d8fb298e2
aish2809 <aishwaryrastogi28@gmail.com>
2021-05-29 07:12:33 -0400

    Changed feature test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         	Bye! there
+         	Hi! Aishwary Rastogi
         </p>
         <a
           className="App-link"
